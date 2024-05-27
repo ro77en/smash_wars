61a992a3d53fc22904fb8a07a34e5380590dada4
ro77en <lucas.mdantas@icloud.com>
2024-05-27 14:56:58 -0300

    frog chase logic

--- Frog.gd
+++ Frog.gd
@@ -1,11 +1,33 @@
 extends CharacterBody2D
 
+var SPEED = 50
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var player
+var chase = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _physics_process(delta):
+	# Gravity for Frog
+	velocity.y += gravity + delta
+	if chase == true:
+		get_node("AnimatedSprite2D").play("Jump")
+		player = get_node("../../Player/Player")
+		var direction = (player.position - self.position).normalized()
+		if direction.x > 0:
+			get_node("AnimatedSprite2D").flip_h = true
+		else:
+			get_node("AnimatedSprite2D").flip_h = false
+		velocity.x = direction.x * SPEED
+	else:
+		get_node("AnimatedSprite2D").play("Idle")
+		velocity.x = 0
+	move_and_slide()
+	
 
+func _on_player_detection_body_entered(body):
+	if body.name == "Player":
+		chase = true
+		
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_player_detection_body_exited(body):
+	if body.name == "Player":
+		chase = false
